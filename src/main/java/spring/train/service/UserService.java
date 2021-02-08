9757eb6fb04e9c07abd3968ed2a623730cf850fa
engineer027 <dp270597dma@gmail.com>
2021-02-08 15:22:43 +0200

    config pom create AppConfig update UserService and UserServiceImpl

--- src/main/java/spring/train/service/UserService.java
+++ src/main/java/spring/train/service/UserService.java
@@ -1,8 +1,7 @@
 package spring.train.service;
 
-import spring.train.model.User;
-
 import java.util.List;
+import spring.train.model.User;
 
 public interface UserService {
     void add(User user);
--- src/main/java/spring/train/service/UserServiceImpl.java
+++ src/main/java/spring/train/service/UserServiceImpl.java
@@ -1,10 +1,18 @@
 package spring.train.service;
 
+import java.util.List;
+import org.springframework.stereotype.Service;
+import spring.train.dao.UserDao;
 import spring.train.model.User;
 
-import java.util.List;
+@Service
+public class UserServiceImpl implements UserService {
+    private UserDao userDao;
 
-public class UserServiceImpl implements UserService{
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
     @Override
     public void add(User user) {
 
